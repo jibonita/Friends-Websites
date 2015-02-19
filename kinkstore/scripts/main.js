bf946591c64b5e839091f4f73272c13a89017194
jibonita <bamishore@gmail.com>
2015-02-19 15:20:53 +0200

    MQ menu test

--- kinkstore/scripts/main.js
+++ kinkstore/scripts/main.js
@@ -1,14 +1,16 @@
 (function() {
 
-    $('.navbar-fluid button').click(function() {
+	$('.navbar-fluid button').click(function() {
     	var speed = 800;
         var clicks = $(this).data('clicks');
         if (clicks) {
             //** hide menu
             $('#menu').hide(speed/3);
+            alert('hide');
         } else {
             //** display menu
             $('#menu').show(speed);
+            alert('show');
         }
         $(this).data("clicks", !clicks);
     });
