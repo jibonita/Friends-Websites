28167f3284282a7fa8cc351c0f6e4e082fb94a44
jibonita <bamishore@gmail.com>
2015-02-21 23:09:35 +0200

    MQ menu test in FF mobile

--- kinkstore/scripts/main.js
+++ kinkstore/scripts/main.js
@@ -2,23 +2,45 @@
 
     $('.navbar-fluid button').click(function() {
 
-        var speed = 800;
-        var clicks = $(this).data('clicks');
-        alert(!clicks)
-        $('#searchfor').val(clicks);
+        setTimeout(function() {
 
-        if (!clicks) {
-            //** display menu
-            $('#menu').show(speed);
-            // $('#searchfor').val('show')
-        } else {
+            var speed = 800;
+            var clicks = $(this).data('clicks');
 
-            //** hide menu
-            $('#menu').hide(speed);
-            // $('#searchfor').val('hide')
-        }
+            $('#searchfor').val(clicks);
 
-        $(this).data('clicks', !clicks);
+            if (!clicks) {
+                //** display menu
+                $('#menu').show(speed);
+                // $('#searchfor').val('show')
+            } else {
+
+                //** hide menu
+                $('#menu').hide(speed);
+                // $('#searchfor').val('hide')
+            }
+
+            $(this).data('clicks', !clicks);
+
+        }, 1000);
+
+        // var speed = 800;
+        // var clicks = $(this).data('clicks');
+
+        // $('#searchfor').val(clicks);
+
+        // if (!clicks) {
+        //     //** display menu
+        //     $('#menu').show(speed);
+        //     // $('#searchfor').val('show')
+        // } else {
+
+        //     //** hide menu
+        //     $('#menu').hide(speed);
+        //     // $('#searchfor').val('hide')
+        // }
+
+        // $(this).data('clicks', !clicks);
     });
 
 })();
