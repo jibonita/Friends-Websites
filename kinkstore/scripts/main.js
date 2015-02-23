88e2dd24d4ab7e07d94b83d83c8f1f380825173b
jibonita <bamishore@gmail.com>
2015-02-23 12:54:21 +0200

    MQ search box update

--- kinkstore/scripts/main.js
+++ kinkstore/scripts/main.js
@@ -3,53 +3,17 @@ $(document).ready(function() {
     var isMenuOpen = false;
     $('.navbar-fluid button.navbar-toggle').click(function() {
 
-
         var speed = 800;
-        // var clicks = $(this).data('clicks');
 
-        // $('#searchfor').val((isMenuOpen?'X:':'O:')+new Date($.now()));
-        // clicks = isMenuOpen;
-        
         if (!isMenuOpen) {
             //** display menu
             $('#menu').slideDown(speed);
-            // setTimeout(function() {
-            //     $('#menu').css('display', 'block');
-            // }, 500);
-
-            // $('#searchfor').val('show')
         } else {
-
             //** hide menu
-            $('#menu').slideUp(speed/3);
-            // setTimeout(function() {
-            //     $('#menu').css('display', 'none');
-            // }, 500);
-
-            // $('#searchfor').val('hide')
+            $('#menu').slideUp(speed / 3);
         }
 
-        //$(this).data('clicks', !clicks);
         isMenuOpen = !isMenuOpen;
-
-
-        // var speed = 800;
-        // var clicks = $(this).data('clicks');
-
-        // $('#searchfor').val(clicks);
-
-        // if (!clicks) {
-        //     //** display menu
-        //     $('#menu').show(speed);
-        //     // $('#searchfor').val('show')
-        // } else {
-
-        //     //** hide menu
-        //     $('#menu').hide(speed);
-        //     // $('#searchfor').val('hide')
-        // }
-
-        // $(this).data('clicks', !clicks);
     });
 
 });
