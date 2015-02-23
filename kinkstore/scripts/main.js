07622409eb8278a7675200a092b57fe54f229e17
jibonita <bamishore@gmail.com>
2015-02-23 09:53:20 +0200

    MQ menu FF mobile test

--- kinkstore/scripts/main.js
+++ kinkstore/scripts/main.js
@@ -1,29 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $('.navbar-fluid button').click(function() {
+    $('.navbar-fluid button.navbar-toggle').click(function() {
 
-        
-            var speed = 800;
-            var clicks = $(this).data('clicks');
 
-            $('#searchfor').val(clicks);
+        var speed = 800;
+        var clicks = $(this).data('clicks');
 
-            if (!clicks) {
-                //** display menu
-                //$('#menu').show(speed);
+        $('#searchfor').val(clicks);
+
+        if (!clicks) {
+            //** display menu
+            //$('#menu').show(speed);
+            setTimeout(function() {
                 $('#menu').css('display', 'block');
-                // $('#searchfor').val('show')
-            } else {
+            }, 500);
 
-                //** hide menu
-                //$('#menu').hide(speed);
+            // $('#searchfor').val('show')
+        } else {
+
+            //** hide menu
+            //$('#menu').hide(speed);
+            setTimeout(function() {
                 $('#menu').css('display', 'none');
-                // $('#searchfor').val('hide')
-            }
+            }, 500);
 
-            $(this).data('clicks', !clicks);
+            // $('#searchfor').val('hide')
+        }
 
-        
+        $(this).data('clicks', !clicks);
+
+
 
         // var speed = 800;
         // var clicks = $(this).data('clicks');
